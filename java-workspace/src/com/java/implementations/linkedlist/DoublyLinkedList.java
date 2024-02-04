98484f30fad65e200955ca302525d4c4f8291bdd
Shrinivas Joshi <shrinivas23.git@gmail.com>
2024-02-04 18:06:45 +0530

    Doubly Linked List Implementations
    Implemented Doubly Linked Lists Operations :
    1. Swapping First and Last
    2. Reversing The List

---
+++ java-workspace/src/com/java/implementations/linkedlist/DoublyLinkedList.java
@@ -0,0 +1,102 @@
+package com.java.implementations.linkedlist;
+
+public class DoublyLinkedList {
+
+    private Node head;
+    private Node tail;
+    private int length = 0;
+
+    class Node{
+        Node next;
+        Node previous;
+        int value;
+
+        public Node(int value) {
+            this.value  = value;
+        }
+    }
+
+    public DoublyLinkedList(int value) {
+        Node newNode = new Node(value);
+        head = tail = newNode;
+        length++;
+    }
+
+    public void printHeadValue(){
+        if (head != null) {
+            System.out.println("Head: " + head.value);
+        }else {
+            System.out.println("Head is null.");
+        }
+    }
+
+    public void printTailValue(){
+        if (head != null) {
+            System.out.println("Tail: " + tail.value);
+        }else {
+            System.out.println("Tail is null.");
+        }
+    }
+
+    public void getLength(){
+        if (head != null) {
+            System.out.println("Length: " + head.value);
+        }else {
+            System.out.println("List is empty.");
+        }
+    }
+
+    public void addToListAtEnd(int value){
+        Node newNode = new Node(value);
+        tail.next = newNode;
+        newNode.previous = tail;
+        tail = newNode;
+        length++;
+    }
+
+    public void printList(){
+        Node temp = head;
+        while (temp != null){
+            if (temp.next != null){
+                System.out.print(temp.value+" <-> ");
+            }else {
+                System.out.println(temp.value+" <-> null\n");
+            }
+            temp = temp.next;
+        }
+    }
+
+    /*
+     * Scenario 1 : Swap First and Last Item in a Doubly Linked List
+     *      => If the length of the list is less than 2 then we do not swap.
+     * */
+    public boolean swapFirstLast(){
+        if (length < 2) return false;
+        int tempValue = head.value;
+        head.value = tail.value;
+        tail.value = tempValue;
+        return true;
+
+    }
+
+    /*
+     * Scenario 2 : Reverse The DLL
+     *
+     * */
+    public void reverseList(){
+        if (length <= 1) {
+            System.out.println("List has only item. Cannot be reversed.");
+        } else{
+            Node temp = tail;
+            tail = head;
+            head = temp;
+            while (temp != null){
+                temp.next = temp.previous;
+                temp = temp.previous;
+            }
+        }
+    }
+
+    
+
+}
---
+++ java-workspace/src/com/java/implementations/linkedlist/DoublyLinkedListImpl.java
@@ -0,0 +1,44 @@
+package com.java.implementations.linkedlist;
+
+public class DoublyLinkedListImpl {
+    public static void main(String[] args) {
+
+        DoublyLinkedList dll = new DoublyLinkedList(2);
+        dll.addToListAtEnd(3);
+        dll.addToListAtEnd(4);
+        dll.addToListAtEnd(5);
+        dll.addToListAtEnd(6);
+        dll.addToListAtEnd(7);
+
+        dll.printHeadValue();
+        dll.printTailValue();
+        dll.getLength();
+
+        System.out.println("\nDoubly Linked List: ");
+        dll.printList();
+
+        /*
+         * Scenario 1 : Swap First and Last Item in a Doubly Linked List
+         *
+         * */
+        dll.swapFirstLast();
+
+        dll.printHeadValue();
+        dll.printTailValue();
+        dll.getLength();
+
+        System.out.println("\nDoubly Linked List: ");
+        dll.printList();
+
+        dll.reverseList();
+
+        dll.printHeadValue();
+        dll.printTailValue();
+        dll.getLength();
+
+        System.out.println("\nDoubly Linked List: ");
+        dll.printList();
+
+
+    }
+}
